9b2642751de77ce82a8763235d50506ddd46354d
VadimRustam <vg1226205@gmail.com>
2024-12-26 19:42:04 +0500

    f

---
+++ client/admin/main.js
@@ -0,0 +1,3 @@
+function Buttom_admin_bw(){
+    window.location.href='../index.html';
+}
---
+++ client/logica.js
@@ -0,0 +1,6 @@
+function Buttom_user(){
+    window.location.href='user/index.html';
+}
+function Button_admin(){
+    window.location.href='admin/index.html';
+}
